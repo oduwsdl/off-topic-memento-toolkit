1d2f0d89f79e614b8acc60052329be0325647854
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-02-25 09:35:30 -0700

    added a test for empty source documents in the middle of a set

--- tests/timemap_measures_test.py
+++ tests/timemap_measures_test.py
@@ -683,3 +683,63 @@ class TestingTimeMapMeasures(unittest.TestCase):
         
         self.assertEqual(expected_scores, scores)
 
+    def test_empty_document_in_middle(self):
+
+        working_directory = "/tmp/test_empty_documents"
+
+        if os.path.exists(working_directory):
+            shutil.rmtree(working_directory)
+
+        cm = collectionmodel.CollectionModel(working_directory=working_directory)
+
+        headers = {
+            "key1": "value1",
+            "key2": "value2"
+        }
+
+        timemap_content ="""<original1>; rel="original",
+<timemap1>; rel="self"; type="application/link-format"; from="Tue, 21 Mar 2016 15:45:06 GMT"; until="Tue, 21 Mar 2018 15:45:12 GMT",
+<timegate1>; rel="timegate",
+<memento11>; rel="first memento"; datetime="Tue, 21 Jan 2016 15:45:06 GMT",
+<memento12>; rel="memento"; datetime="Tue, 21 Jan 2017 15:45:06 GMT",
+<memento13>; rel="last memento"; datetime="Tue, 21 Jan 2018 15:45:12 GMT"
+"""
+
+        full_html_document = b"<html>The quick brown fox jumps over the lazy dog<body></html>"
+        empty_html_document = b"<html><body></body></html>"
+
+        cm.addTimeMap("timemap1", timemap_content, headers)
+        cm.addMemento("memento11", full_html_document, headers)
+        cm.addMemento("memento12", empty_html_document, headers)
+        cm.addMemento("memento13", full_html_document, headers)
+
+        scores = compute_jaccard_across_TimeMap(
+            cm, scores=None, tokenize=None, stemming=True)
+
+        pp.pprint(scores)
+
+        # Rather than dealing with empty documents, this throws
+        # ValueError: empty vocabulary; perhaps the documents only contain stop words
+        # it should handle the error gracefully
+        scores = compute_cosine_across_TimeMap(
+            cm, scores=None, tokenize=None, stemming=True)
+
+        pp.pprint(scores)
+
+        expected_scores = {   'timemaps': {   'timemap1': {   
+                                    'memento11': {   'timemap measures': {   'cosine': {   'boilerplate removal': True,
+                                                                                           'comparison score': 1.0000000000000002,
+                                                                                           'stemmed': True,
+                                                                                           'tokenized': True}}},
+                                    'memento12': {   'timemap measures': {   'cosine': {   'boilerplate removal': True,
+                                                                                           'comparison score': 0.0,
+                                                                                           'stemmed': True,
+                                                                                           'tokenized': True}}},
+                                    'memento13': {   'timemap measures': {   'cosine': {   'boilerplate removal': True,
+                                                                                           'comparison score': 1.0000000000000002,
+                                                                                           'stemmed': True,
+                                                                                           'tokenized': True}}}}}}
+
+        
+        self.assertEqual(expected_scores, scores)
+
